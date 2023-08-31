6cdcbc522731600d94f3642790c389ebf6ace9e3
jost3 <1427593@senati.pe>
2023-08-31 17:02:32 -0500

    se da la funcion a los bootnes q tinen un numero para mostrar en pantalla

--- calculadora_c/Form1.Designer.cs
+++ calculadora_c/Form1.Designer.cs
@@ -28,215 +28,220 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button7 = new Button();
-            button8 = new Button();
-            button9 = new Button();
-            button10 = new Button();
-            button11 = new Button();
-            button12 = new Button();
-            button13 = new Button();
-            button15 = new Button();
-            button16 = new Button();
-            button19 = new Button();
-            button20 = new Button();
-            button14 = new Button();
+            btn_sum = new Button();
+            btn0 = new Button();
+            btn7 = new Button();
+            btn4 = new Button();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn5 = new Button();
+            btn8 = new Button();
+            btn_punto = new Button();
+            btn_rest = new Button();
+            btn3 = new Button();
+            btn6 = new Button();
+            btn9 = new Button();
+            btn_mult = new Button();
+            btn_igual = new Button();
+            btn_elim = new Button();
+            btn_div = new Button();
+            btn_retroceder = new Button();
             tbx = new TextBox();
             SuspendLayout();
             // 
-            // button1
+            // btn_sum
             // 
-            button1.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(24, 91);
-            button1.Name = "button1";
-            button1.Size = new Size(45, 45);
-            button1.TabIndex = 0;
-            button1.Text = "+";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            btn_sum.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_sum.Location = new Point(23, 145);
+            btn_sum.Name = "btn_sum";
+            btn_sum.Size = new Size(45, 45);
+            btn_sum.TabIndex = 0;
+            btn_sum.Text = "+";
+            btn_sum.UseVisualStyleBackColor = true;
+            btn_sum.Click += btn1_Click;
             // 
-            // button2
+            // btn0
             // 
-            button2.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.Location = new Point(24, 299);
-            button2.Name = "button2";
-            button2.Size = new Size(109, 45);
-            button2.TabIndex = 1;
-            button2.Text = "0";
-            button2.UseVisualStyleBackColor = true;
+            btn0.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn0.Location = new Point(23, 353);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(109, 45);
+            btn0.TabIndex = 1;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btn7
             // 
-            button3.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button3.Location = new Point(24, 146);
-            button3.Name = "button3";
-            button3.Size = new Size(45, 45);
-            button3.TabIndex = 2;
-            button3.Text = "7";
-            button3.UseVisualStyleBackColor = true;
+            btn7.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn7.Location = new Point(23, 200);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(45, 45);
+            btn7.TabIndex = 2;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
             // 
-            // button4
+            // btn4
             // 
-            button4.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button4.Location = new Point(24, 197);
-            button4.Name = "button4";
-            button4.Size = new Size(45, 45);
-            button4.TabIndex = 3;
-            button4.Text = "4";
-            button4.UseVisualStyleBackColor = true;
-            button4.Click += button4_Click;
+            btn4.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn4.Location = new Point(23, 251);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(45, 45);
+            btn4.TabIndex = 3;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn2_Click;
             // 
-            // button5
+            // btn1
             // 
-            button5.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button5.Location = new Point(24, 248);
-            button5.Name = "button5";
-            button5.Size = new Size(45, 45);
-            button5.TabIndex = 4;
-            button5.Text = "1";
-            button5.UseVisualStyleBackColor = true;
+            btn1.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn1.Location = new Point(23, 302);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(45, 45);
+            btn1.TabIndex = 4;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // btn2
             // 
-            button6.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button6.Location = new Point(88, 248);
-            button6.Name = "button6";
-            button6.Size = new Size(45, 45);
-            button6.TabIndex = 9;
-            button6.Text = "2";
-            button6.UseVisualStyleBackColor = true;
-            button6.Click += button6_Click;
+            btn2.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn2.Location = new Point(87, 302);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(45, 45);
+            btn2.TabIndex = 9;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn3_Click;
             // 
-            // button7
+            // btn5
             // 
-            button7.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button7.Location = new Point(88, 197);
-            button7.Name = "button7";
-            button7.Size = new Size(45, 45);
-            button7.TabIndex = 8;
-            button7.Text = "5";
-            button7.UseVisualStyleBackColor = true;
+            btn5.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn5.Location = new Point(87, 251);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(45, 45);
+            btn5.TabIndex = 8;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
             // 
-            // button8
+            // btn8
             // 
-            button8.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button8.Location = new Point(88, 146);
-            button8.Name = "button8";
-            button8.Size = new Size(45, 45);
-            button8.TabIndex = 7;
-            button8.Text = "8";
-            button8.UseVisualStyleBackColor = true;
-            button8.Click += button8_Click;
+            btn8.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn8.Location = new Point(87, 200);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(45, 45);
+            btn8.TabIndex = 7;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn4_Click;
             // 
-            // button9
+            // btn_punto
             // 
-            button9.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button9.Location = new Point(151, 299);
-            button9.Name = "button9";
-            button9.Size = new Size(45, 45);
-            button9.TabIndex = 6;
-            button9.Text = ".";
-            button9.UseVisualStyleBackColor = true;
+            btn_punto.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_punto.Location = new Point(150, 353);
+            btn_punto.Name = "btn_punto";
+            btn_punto.Size = new Size(45, 45);
+            btn_punto.TabIndex = 6;
+            btn_punto.Text = ".";
+            btn_punto.UseVisualStyleBackColor = true;
+            btn_punto.Click += btn_punto_Click;
             // 
-            // button10
+            // btn_rest
             // 
-            button10.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button10.Location = new Point(88, 95);
-            button10.Name = "button10";
-            button10.Size = new Size(45, 45);
-            button10.TabIndex = 5;
-            button10.Text = "-";
-            button10.UseVisualStyleBackColor = true;
+            btn_rest.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_rest.Location = new Point(87, 149);
+            btn_rest.Name = "btn_rest";
+            btn_rest.Size = new Size(45, 45);
+            btn_rest.TabIndex = 5;
+            btn_rest.Text = "-";
+            btn_rest.UseVisualStyleBackColor = true;
             // 
-            // button11
+            // btn3
             // 
-            button11.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button11.Location = new Point(151, 248);
-            button11.Name = "button11";
-            button11.Size = new Size(45, 45);
-            button11.TabIndex = 14;
-            button11.Text = "3";
-            button11.UseVisualStyleBackColor = true;
+            btn3.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn3.Location = new Point(150, 302);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(45, 45);
+            btn3.TabIndex = 14;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
             // 
-            // button12
+            // btn6
             // 
-            button12.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button12.Location = new Point(151, 197);
-            button12.Name = "button12";
-            button12.Size = new Size(45, 45);
-            button12.TabIndex = 13;
-            button12.Text = "6";
-            button12.UseVisualStyleBackColor = true;
+            btn6.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn6.Location = new Point(150, 251);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(45, 45);
+            btn6.TabIndex = 13;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
             // 
-            // button13
+            // btn9
             // 
-            button13.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button13.Location = new Point(151, 146);
-            button13.Name = "button13";
-            button13.Size = new Size(45, 45);
-            button13.TabIndex = 12;
-            button13.Text = "9";
-            button13.UseVisualStyleBackColor = true;
+            btn9.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn9.Location = new Point(150, 200);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(45, 45);
+            btn9.TabIndex = 12;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
             // 
-            // button15
+            // btn_mult
             // 
-            button15.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button15.Location = new Point(151, 95);
-            button15.Name = "button15";
-            button15.Size = new Size(45, 45);
-            button15.TabIndex = 10;
-            button15.Text = "*";
-            button15.UseVisualStyleBackColor = true;
+            btn_mult.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_mult.Location = new Point(150, 149);
+            btn_mult.Name = "btn_mult";
+            btn_mult.Size = new Size(45, 45);
+            btn_mult.TabIndex = 10;
+            btn_mult.Text = "*";
+            btn_mult.UseVisualStyleBackColor = true;
             // 
-            // button16
+            // btn_igual
             // 
-            button16.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button16.Location = new Point(215, 248);
-            button16.Name = "button16";
-            button16.Size = new Size(45, 96);
-            button16.TabIndex = 19;
-            button16.Text = "=";
-            button16.UseVisualStyleBackColor = true;
+            btn_igual.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_igual.Location = new Point(214, 302);
+            btn_igual.Name = "btn_igual";
+            btn_igual.Size = new Size(45, 96);
+            btn_igual.TabIndex = 19;
+            btn_igual.Text = "=";
+            btn_igual.UseVisualStyleBackColor = true;
             // 
-            // button19
+            // btn_elim
             // 
-            button19.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button19.Location = new Point(215, 146);
-            button19.Name = "button19";
-            button19.Size = new Size(45, 45);
-            button19.TabIndex = 16;
-            button19.Text = "CE";
-            button19.UseVisualStyleBackColor = true;
+            btn_elim.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_elim.Location = new Point(214, 200);
+            btn_elim.Name = "btn_elim";
+            btn_elim.Size = new Size(45, 45);
+            btn_elim.TabIndex = 16;
+            btn_elim.Text = "CE";
+            btn_elim.UseVisualStyleBackColor = true;
             // 
-            // button20
+            // btn_div
             // 
-            button20.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button20.Location = new Point(215, 95);
-            button20.Name = "button20";
-            button20.Size = new Size(45, 45);
-            button20.TabIndex = 15;
-            button20.Text = "/";
-            button20.UseVisualStyleBackColor = true;
+            btn_div.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_div.Location = new Point(214, 149);
+            btn_div.Name = "btn_div";
+            btn_div.Size = new Size(45, 45);
+            btn_div.TabIndex = 15;
+            btn_div.Text = "/";
+            btn_div.UseVisualStyleBackColor = true;
             // 
-            // button14
+            // btn_retroceder
             // 
-            button14.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button14.Location = new Point(215, 197);
-            button14.Name = "button14";
-            button14.Size = new Size(45, 45);
-            button14.TabIndex = 20;
-            button14.Text = "<-";
-            button14.UseVisualStyleBackColor = true;
+            btn_retroceder.Font = new Font("Verdana", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btn_retroceder.Location = new Point(214, 251);
+            btn_retroceder.Name = "btn_retroceder";
+            btn_retroceder.Size = new Size(45, 45);
+            btn_retroceder.TabIndex = 20;
+            btn_retroceder.Text = "<-";
+            btn_retroceder.UseVisualStyleBackColor = true;
             // 
             // tbx
             // 
             tbx.Font = new Font("Segoe Print", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            tbx.Location = new Point(24, 30);
+            tbx.Location = new Point(23, 84);
             tbx.Name = "tbx";
             tbx.ReadOnly = true;
             tbx.Size = new Size(236, 50);
@@ -249,51 +254,51 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(372, 425);
             Controls.Add(tbx);
-            Controls.Add(button14);
-            Controls.Add(button16);
-            Controls.Add(button19);
-            Controls.Add(button20);
-            Controls.Add(button11);
-            Controls.Add(button12);
-            Controls.Add(button13);
-            Controls.Add(button15);
-            Controls.Add(button6);
-            Controls.Add(button7);
-            Controls.Add(button8);
-            Controls.Add(button9);
-            Controls.Add(button10);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(btn_retroceder);
+            Controls.Add(btn_igual);
+            Controls.Add(btn_elim);
+            Controls.Add(btn_div);
+            Controls.Add(btn3);
+            Controls.Add(btn6);
+            Controls.Add(btn9);
+            Controls.Add(btn_mult);
+            Controls.Add(btn2);
+            Controls.Add(btn5);
+            Controls.Add(btn8);
+            Controls.Add(btn_punto);
+            Controls.Add(btn_rest);
+            Controls.Add(btn1);
+            Controls.Add(btn4);
+            Controls.Add(btn7);
+            Controls.Add(btn0);
+            Controls.Add(btn_sum);
             Name = "Form1";
             Text = "Form1";
-            Load += Form1_Load;
+            Load += btn0_Click;
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
-        private Button button7;
-        private Button button8;
-        private Button button9;
-        private Button button10;
-        private Button button11;
-        private Button button12;
-        private Button button13;
-        private Button button15;
-        private Button button16;
-        private Button button19;
-        private Button button20;
-        private Button button14;
+        private Button btn_sum;
+        private Button btn0;
+        private Button btn7;
+        private Button btn4;
+        private Button btn1;
+        private Button btn2;
+        private Button btn5;
+        private Button btn8;
+        private Button btn_punto;
+        private Button btn_rest;
+        private Button btn3;
+        private Button btn6;
+        private Button btn9;
+        private Button btn_mult;
+        private Button btn_igual;
+        private Button btn_elim;
+        private Button btn_div;
+        private Button btn_retroceder;
         private TextBox tbx;
     }
 }
--- calculadora_c/Form1.cs
+++ calculadora_c/Form1.cs
@@ -2,33 +2,67 @@ namespace calculadora_c
 {
     public partial class Form1 : Form
     {
+        double primero;
+        double segundo;
+        double operador;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btn0_Click(object sender, EventArgs e)
         {
-
+            tbx.Text = tbx.Text + "0";
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void btn1_Click(object sender, EventArgs e)
         {
-
+            tbx.Text = tbx.Text + "1";
         }
 
-        private void button8_Click(object sender, EventArgs e)
+        private void btn2_Click(object sender, EventArgs e)
         {
-
+            tbx.Text = tbx.Text + "2";
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btn3_Click(object sender, EventArgs e)
         {
-
+            tbx.Text = tbx.Text + "3";
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void btn4_Click(object sender, EventArgs e)
         {
+            tbx.Text = tbx.Text + "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            tbx.Text = tbx.Text + "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            tbx.Text = tbx.Text + "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            tbx.Text = tbx.Text + "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            tbx.Text = tbx.Text + "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            tbx.Text = tbx.Text + "9";
+        }
+
+        private void btn_punto_Click(object sender, EventArgs e)
+        {
+            tbx.Text = tbx.Text + ".";
 
         }
     }
